6028657d276ea26db8f3406dafa8d85e3edbb075
rvan-der <remy_v.d.zijden@hotmail.fr>
2022-09-11 21:20:24 +0200

    visualization almost finished

--- visualizer/command_widget.py
+++ visualizer/command_widget.py
@@ -1,21 +1,56 @@
-from PySide6.QtWidgets import QWidget, QPushButton, QGridLayout, QLabel, QSpinBox
-from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QPushButton, QGridLayout, QLabel, QSpinBox, QFrame, QSizePolicy
+from PySide6.QtCore import Qt, Slot
 
 
-class CommandWidget(QWidget):
+class CommandWidget(QFrame):
 
-	def __init__(self):
-		QWidget.__init__(self)
+	def __init__(self, theta0, theta1):
+		QFrame.__init__(self)
+
+		self.theta0 = theta0
+		self.theta1 = theta1
+
+		self.setFrameStyle(QFrame.StyledPanel | QFrame.Raised)
+
 		self.layout = QGridLayout()
-		self.layout.setAlignment(Qt.AlignTop)
-		self.trainButton = QPushButton("Launch Training")
+		self.layout.setAlignment(Qt.AlignTop | Qt.AlignLeft)
+		self.layout.setVerticalSpacing(30)
+		self.layout.setContentsMargins(15, 50, 15, 50)
+
 		self.trainLabel = QLabel("Number of iterations (max 10^8)")
+
 		self.trainInput = QSpinBox()
 		self.trainInput.setRange(1,100000000)
 		self.trainInput.setSingleStep(50)
-		self.layout.addWidget(self.trainLabel,0,0,1,1, alignment=Qt.AlignHCenter)
-		self.layout.addWidget(self.trainInput,0,1,1,1, alignment=Qt.AlignHCenter)
-		self.layout.addWidget(self.trainButton,2,0,1,2, alignment=Qt.AlignHCenter)
-		self.layout.setRowMinimumHeight(1,30)
-		self.layout.setContentsMargins(15,30,15,30)
+
+		self.trainButton = QPushButton("Launch Training")
+		self.trainButton.setMaximumHeight(50)
+		self.trainButton.setMinimumHeight(50)
+		self.trainButton.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+		self.trainButton.clicked.connect(self.test)
+
+		self.theta0Label = QLabel("<font size=30 color=red>θ<sub>0</sub> = %.3f</font>"%(self.theta0))
+		self.theta0Label.setTextFormat(Qt.RichText)
+
+		self.theta1Label = QLabel("<font size=30 color=red>θ<sub>1</sub> = %.3f</font>"%(self.theta1))
+		self.theta1Label.setTextFormat(Qt.RichText)
+
+		# thetaFont = self.font()
+		# thetaFont.setPointSize(30)
+		# self.theta0Label.setFont(thetaFont)
+		# self.theta1Label.setFont(thetaFont)
+
+		self.layout.addWidget(self.trainLabel, 0, 0, 1, 1, alignment=Qt.AlignLeft)
+		self.layout.addWidget(self.trainInput, 0, 1, 1, 1, alignment=Qt.AlignRight)
+		self.layout.addWidget(self.trainButton, 1, 0, 1, 2)
+		self.layout.setRowMinimumHeight(2, 80)
+		self.layout.addWidget(self.theta0Label, 3, 0, 1, 1, alignment=Qt.AlignLeft)
+		self.layout.addWidget(self.theta1Label, 4, 0, 1, 1, alignment=Qt.AlignLeft)
+		
 		self.setLayout(self.layout)
+
+
+	@Slot()
+	def test(self):
+		print(self.theta0Label.text())
+		print(self.theta1Label.text())
--- visualizer/main_widget.py
+++ visualizer/main_widget.py
@@ -16,35 +16,50 @@ class MainWidget(QWidget):
 
 		self.data = data
 		self.weights = weights
+		self.theta0 = 0
+		self.theta1 = 0
+		self.calculate_thetas()
 		self.normalizedData = normalize_data(self.data, self.weights)
 		self.dataSeries = None
+		self.modelSeries = None
 
-		self.layout = QHBoxLayout()
+		self.mainLayout = QHBoxLayout()
 
 		self.chartView = QChartView()
 		self.chart = self.chartView.chart()
 
 		self.yAxis = QValueAxis()
-		self.yAxis.setRange(0, 10000)
+		self.yAxis.setTitleText("Price")
+		self.yAxis.setLabelFormat("%d")
+		self.yAxis.setRange(-1000, 10000)
 		self.yAxis.setTickType(QValueAxis.TicksDynamic)
-		self.yAxis.setTickAnchor(0)
+		self.yAxis.setTickAnchor(-1000)
 		self.yAxis.setTickInterval(1000)
 
 		self.xAxis = QValueAxis()
-		self.xAxis.setRange(20000, 260000)
+		self.xAxis.setTitleText("Distance (km)")
+		self.xAxis.setLabelFormat("%d")
+		self.xAxis.setLabelsAngle(-45)
+		self.xAxis.setRange(0, 260000)
 		self.xAxis.setTickType(QValueAxis.TicksDynamic)
-		self.xAxis.setTickAnchor(20000)
+		self.xAxis.setTickAnchor(0)
 		self.xAxis.setTickInterval(20000)
+
+		tickLabelsFont = self.font()
+		tickLabelsFont.setPointSize(7)
+		self.xAxis.setLabelsFont(tickLabelsFont)
+		self.yAxis.setLabelsFont(tickLabelsFont)
 
 		self.chart.addAxis(self.xAxis, Qt.AlignBottom)
 		self.chart.addAxis(self.yAxis, Qt.AlignLeft)
 
-		self.commandWidget = CommandWidget()
+		self.commandWidget = CommandWidget(self.theta0, self.theta1)
 
-		self.layout.addWidget(self.commandWidget, stretch=1)
-		self.layout.addWidget(self.chartView, stretch=3)
-		self.setLayout(self.layout)
+		self.mainLayout.addWidget(self.commandWidget, stretch=1)
+		self.mainLayout.addWidget(self.chartView, stretch=3)
+		self.setLayout(self.mainLayout)
 		self.plot_data()
+		self.plot_model()
 
 
 	def plot_data(self):
@@ -53,8 +68,33 @@ class MainWidget(QWidget):
 			self.dataSeries = None
 		self.dataSeries = QScatterSeries()
 		self.dataSeries.setName("Car prices")
+		self.dataSeries.setMarkerShape(QScatterSeries.MarkerShapeRectangle)
+		self.dataSeries.setMarkerSize(8)
 		for car in self.data:
 			self.dataSeries.append(car["km"], car["price"])
 		self.chart.addSeries(self.dataSeries)
 		self.dataSeries.attachAxis(self.xAxis)
 		self.dataSeries.attachAxis(self.yAxis)
+		self.dataSeries.setColor(QColor("blue"))
+
+
+	def plot_model(self):
+		if self.modelSeries != None:
+			self.chart.removeSeries(self.modelSeries)
+			self.modelSeries = None
+		self.modelSeries = QLineSeries()
+		self.modelSeries.setName("Model")
+		self.modelSeries.append(0, self.theta0)
+		self.modelSeries.append(260000, self.theta1 * 260000 + self.theta0)
+		# print(260000, self.theta1 * 260000 + self.theta0)
+		# print(self.theta0, self.theta1)
+		self.chart.addSeries(self.modelSeries)
+		self.modelSeries.attachAxis(self.xAxis)
+		self.modelSeries.attachAxis(self.yAxis)
+		self.modelSeries.setColor(QColor("red"))
+
+
+	def calculate_thetas(self):
+		self.theta0 = self.weights["theta0"] * self.weights["norm_factor"]
+		endPointValue = (self.weights["theta1"] * (260000 / max(1,self.weights["norm_factor"])) + self.weights["theta0"]) * self.weights["norm_factor"]
+		self.theta1 = (endPointValue - self.theta0) / 260000
